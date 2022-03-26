9ef1c6523e0040f4839c1b3e5645c04e7ca908ac
Rajesh Saini <rajeshsaini2115@gmail.com>
2022-03-26 14:52:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ class Config:
     LOG_GROUP=os.environ.get("LOG_GROUP", "")
 
     #Stream 
-    STREAM_URL=os.environ.get("STARTUP_STREAM", "https://www.youtube.com/watch?v=zcrUCvBD16k")
+    STREAM_URL=os.environ.get("STARTUP_STREAM", "https://youtu.be/l-6O3Kfeebw")
    
     #Database
     DATABASE_URI=os.environ.get("DATABASE_URI", None)
